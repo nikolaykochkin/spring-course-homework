3d070537b74e6c0773a5573a9feadbc2d0bcc05b
Николай <nikolaykochkin91@gmail.com>
2021-11-16 20:06:27 +0300

    [ADD] Fixed N+1 problem for books list

--- library/src/main/java/org/example/repository/BookRepository.java
+++ library/src/main/java/org/example/repository/BookRepository.java
@@ -1,8 +1,13 @@
 package org.example.repository;
 
 import org.example.model.Book;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+import java.util.List;
 
+public interface BookRepository extends JpaRepository<Book, Long> {
+    @Override
+    @EntityGraph(value = "books-entity-graph")
+    List<Book> findAll();
 }
